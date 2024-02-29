a8ceb11b9722ef3a3c78fb83900e1bcd2951031b
UtkarshSharma45 <f20212429@pilani.bits-pilani.ac.in>
2024-02-29 23:46:14 +0530

    first set
    first set computed

--- parser.c
+++ parser.c
@@ -70,8 +70,151 @@ void populate()
     }
     fclose(fp);
 }
+void compute_first_set(){
+    for(int i=0;i<NT_SIZE;i++){
+        if(containsEps[i]==1)continue;
+        gSym x;
+        x.isTerminal=false;
+        x.nt=i;
+
+       
+        //printf("Hi");
+        //int epscheck=0;
+        first_set(x);
+         //printf("Bye");
+        
+    }
+}
+
+void first_set(gSym x){
+    
+    
+    //printf("Hello");
+    for(int i=0;i<MAX_SIZE;i++){
+         int flag=0;
+        if(Grammar[i].rule[0].nt==x.nt)
+        {  //printf("%s ",nonTerminals[x.nt]);
+            
+            for(int j=1;j<=Grammar[i].len;j++){
+                if(Grammar[i].rule[j].isTerminal==true){
+                    //printf("Hola");
+                    if(strcmp(Terminals[Grammar[i].rule[j].t],"TK_EPS")==0){
+                        int f=0;
+                        for(int k=0;k<firstSet[x.nt].size;k++){
+                            if(firstSet[x.nt].set[k].t==Grammar[i].rule[j].t){
+                                f=1;
+                                break;
+                            }
+                        }
+                            if(f==0){
+                                firstSet[x.nt].set[firstSet[x.nt].size]=Grammar[i].rule[j];
+                                firstSet[x.nt].size++;
+
+                            }
+                        
+                        
+                        break;
+
+                    }
+                    else{
+                        //printf("%s ",Terminals[Grammar[i].rule[j].t]);
+                        
+                        int f=0;
+                        for(int k=0;k<firstSet[x.nt].size;k++){
+                            if(firstSet[x.nt].set[k].t==Grammar[i].rule[j].t){
+                                f=1;
+                                break;
+                            }
+                        }
+                            if(f==0){
+                                firstSet[x.nt].set[firstSet[x.nt].size]=Grammar[i].rule[j];
+                                firstSet[x.nt].size++;
+
+                            }
+                        break;
+
+                        
+
+                       
+                }
+                }
+                else{
+                    first_set(Grammar[i].rule[j]);
+                    gSym first=Grammar[i].rule[j];
+                    
+                    for(int k=0;k<firstSet[first.nt].size;k++){
+                        int f=0;
+                        if(firstSet[first.nt].set[k].t==TK_EPS){
+                                flag=1;
+                                continue;
+
+                            }
+                        for(int l=0;l<firstSet[x.nt].size;l++){
+                            
+                            if(firstSet[x.nt].set[l].t==firstSet[first.nt].set[k].t){
+                                f=1;
+                                break;
+                            }
+                        }
+                            if(f==0){
+                                firstSet[x.nt].set[firstSet[x.nt].size]=firstSet[first.nt].set[k];
+                                firstSet[x.nt].size++;
+
+                            }
+
+                        
+
+                    }
+                    if(flag==0){
+                        break;
+                    }
+                    
+                }
+                
+
+
+
+            } 
+            if(flag==1){
+                gSym eps;
+                eps.isTerminal=true;
+                eps.t=TK_EPS;
+                firstSet[x.nt].set[firstSet[x.nt].size]=eps;
+            }
+            
+            containsEps[x.nt]=1;
+            
+            
+            
+        }
+    }
+    
+}
 int main()
 {
     populate();
+    // printf("%s",Terminals[Grammar[MAX_SIZE-1].rule[Grammar[MAX_SIZE-1].len-1].t]);
+    //compute_first_set();
+    compute_first_set();
+    int k=0;
+    for(int i=0;i<NT_SIZE;i++){
+        printf("%i %s ",i,nonTerminals[i]);
+        for(int j=0;j<firstSet[i].size;j++){
+            printf("%s ",Terminals[firstSet[i].set[j].t]);
+            //printf("Yo");
+        }
+        k++;
+        printf("\n");
+    }
+    //printf("%d",k);
+
+     
+    
+    
+    //printf("%d",firstSet[0].set[0].t);
+    //populate();
+    //printf("%d",firstSet[0][0].isTerminal);
+    //firstSet[0].size++;
+    //printf("%d",firstSet[0].size);
 }
 
